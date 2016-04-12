7032849977ec7c35739c3d75bdd544469b3e1f88
stellasu168 <millionstars@gmail.com>
2016-04-12 00:00:43 -0700

    first commit

---
+++ movieMagic/MovieTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MovieTableViewCell.swift
+//  movieMagic
+//
+//  Created by Stella Su on 4/11/16.
+//  Copyright © 2016 Million Stars, LLC. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
