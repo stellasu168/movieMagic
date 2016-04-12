06da111e8d0de3555b3be101c7ba0b3e1656d667
stellasu168 <millionstars@gmail.com>
2016-04-12 05:45:30 -0700

    Adding Protytype Cell and Movie model

---
+++ movieMagic/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  movieMagic
+//
+//  Created by Stella Su on 4/12/16.
+//  Copyright © 2016 Million Stars, LLC. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let title: String
+    let description: String
+    let posterURL: NSURL
+}
--- movieMagic/MovieTableViewCell.swift
+++ movieMagic/MovieTableViewCell.swift
@@ -2,20 +2,32 @@
 //  MovieTableViewCell.swift
 //  movieMagic
 //
-//  Created by Stella Su on 4/11/16.
+//  Created by Stella Su on 4/12/16.
 //  Copyright © 2016 Million Stars, LLC. All rights reserved.
 //
 
 import UIKit
 
-class MovieTableViewCell: UIView {
+class MovieTableViewCell: UITableViewCell {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    var model: Movie? {
+        didSet {
+            guard let newMovie = self.model else  { return }
+            self.movieTitleLabel.text = newMovie.title
+            self.movieDescriptionLabel.text = newMovie.description
+            self.moviePosterImageView.image = .None
+        }
     }
-    */
+    
+    @IBOutlet private weak var moviePosterImageView: UIImageView!
+    @IBOutlet private weak var movieTitleLabel: UILabel!
+    @IBOutlet private weak var movieDescriptionLabel: UILabel!
+    
+    func updateDisplayImages(newImage: UIImage) {
+        self.moviePosterImageView.image = newImage
+    }
+    
+    var posterURL: NSURL?
+
 
 }
