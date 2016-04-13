5b12778aa35bd0fe90f3bf4e37e9e82967331317
stellasu168 <millionstars@gmail.com>
2016-04-12 18:10:07 -0700

    Adding extension for Movie model

--- movieMagic/Movie.swift
+++ movieMagic/Movie.swift
@@ -13,3 +13,28 @@ struct Movie {
     let description: String
     let posterURL: NSURL
 }
+
+extension Movie {
+    static func moviesFromDictionaryArray(dictionaryArray: [NSDictionary]) -> [Movie]? {
+        var movieArray = [Movie]()
+        
+        for dictionary in dictionaryArray {
+            if let posterDictionary = dictionary["posters"] as? NSDictionary,
+                let posterURLString = posterDictionary["thumbnail"] as? String,
+                let posterURL = NSURL(string: posterURLString) {
+                    
+                    let movieTitle = dictionary["title"] as? String ?? "Title Not Found"
+                    let movieDescription = dictionary["synopsis"] as? String ?? "Synopsys Not Found"
+                    
+                    movieArray += [Movie(title: movieTitle, description: movieDescription, posterURL: posterURL)]
+            }
+        }
+        
+        if movieArray.isEmpty == false {
+            return movieArray
+        } else {
+            return .None
+        }
+    }
+}
+
