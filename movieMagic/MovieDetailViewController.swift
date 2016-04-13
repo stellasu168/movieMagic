0d13c1b55854ca19aaa948b66bfce3887842381b
stellasu168 <millionstars@gmail.com>
2016-04-13 05:07:56 -0700

    Added movie detail view controller

---
+++ movieMagic/MovieDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MovieDetailViewController.swift
+//  movieMagic
+//
+//  Created by Stella Su on 4/13/16.
+//  Copyright © 2016 Million Stars, LLC. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    @IBOutlet weak var moviePoster: UIImageView!
+    @IBOutlet weak var movieDetailView: UIScrollView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
